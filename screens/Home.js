e81912d7e74faeeaf65ba0e6d504f95c4984965f
ericnkaka <nkakaeric96@gmail.com>
2023-11-26 19:52:50 +0200

    feat:done

--- screens/Home.js
+++ screens/Home.js
@@ -1,11 +1,28 @@
 import { FlatList, StyleSheet } from 'react-native'
-import React from 'react'
+import React, {useCallback, useState, useEffect} from 'react'
 import PlatePreview from '../components/PlatePreview'
 
 export default function Home({ navigation }) {
+
+    const [ColorPalette, setColorPalette] = useState([])
+
+    const fetchColorPalatte = useCallback(async () => {
+        const result = await fetch('https://color-palette-api.kadikraman.vercel.app/palettes');
+        if(result.ok){
+            const palettes = await result.json();
+            setColorPalette(palettes)
+        }
+    }, []);
+
+    useEffect(() => {
+      fetchColorPalatte()
+      return () => { 
+      }
+    }, [])
+
     return (
         <FlatList
-            data={COLOR_PALETTES}
+            data={ColorPalette}
             style={styles.container}
             keyExtractor={item => item.paletteName}
             renderItem={({ item }) => (
@@ -21,46 +38,6 @@ export default function Home({ navigation }) {
 
     )
 }
-const RAINBOW = [
-    { colorName: 'Red', hexCode: '#FF0000' },
-    { colorName: 'Orange', hexCode: '#FF7F00' },
-    { colorName: 'Yellow', hexCode: '#FFFF00' },
-    { colorName: 'Green', hexCode: '#00FF00' },
-    { colorName: 'Violet', hexCode: '#8B00FF' },
-];
-
-const THE_GYM = [
-    { colorName: 'Red', hexCode: '#c02d28' },
-    { colorName: 'Black', hexCode: '#3e3e3e' },
-    { colorName: 'Grey', hexCode: '#8a8a8a' },
-    { colorName: 'White', hexCode: '#ffffff' },
-    { colorName: 'Orange', hexCode: '#e66225' },
-];
-const COLORS = [
-    { colorName: 'Base03', hexCode: '#002b36' },
-    { colorName: 'Base02', hexCode: '#073642' },
-    { colorName: 'Base01', hexCode: '#586e75' },
-    { colorName: 'Base00', hexCode: '#657b83' },
-    { colorName: 'Base0', hexCode: '#839496' },
-    { colorName: 'Base1', hexCode: '#93a1a1' },
-    { colorName: 'Base2', hexCode: '#eee8d5' },
-    { colorName: 'Base3', hexCode: '#fdf6e3' },
-    { colorName: 'Yellow', hexCode: '#b58900' },
-    { colorName: 'Orange', hexCode: '#cb4b16' },
-    { colorName: 'Red', hexCode: '#dc322f' },
-    { colorName: 'Magenta', hexCode: '#d33682' },
-    { colorName: 'Violet', hexCode: '#6c71c4' },
-    { colorName: 'Blue', hexCode: '#268bd2' },
-    { colorName: 'Cyan', hexCode: '#2aa198' },
-    { colorName: 'Green', hexCode: '#859900' },
-];
-
-const COLOR_PALETTES = [
-    { paletteName: 'Solarized', colors: COLORS },
-    { paletteName: 'The Gym Colors', colors: THE_GYM },
-    { paletteName: 'Rainbow', colors: RAINBOW },
-];
-
 const styles = StyleSheet.create({
     container: {
         backgroundColor: 'white',
